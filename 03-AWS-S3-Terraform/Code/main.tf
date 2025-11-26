72e481cbef37daae071c80284f867a1b5a849194
kutt27 <satheesanamal6@gmail.com>
2025-11-26 22:04:19 +0530

    Day 3 code done

---
+++ 03-AWS-S3-Terraform/Code/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # locking the provider version to 6.x
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # the service will run on us-east-1
+}
+
+# create a s3 bucket
+resource "aws_s3_bucket" "first_s3_bucket" {
+  bucket = "amal-s3-bucket-terraform30days" # name of the bucket, unique one
+
+  tags = {
+    Name        = "My bucket 2.0"
+    Environment = "Dev"
+  }
+}
