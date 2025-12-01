c31650f800c7a950c04113cd13e062e2e7333136
kutt27 <satheesanamal6@gmail.com>
2025-12-01 22:04:25 +0530

    updated the repo

--- 06-Terraform-Project-Structure/Code/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket      = "erraform-state-1754513244"
-    key         = "dev/terraform.tfstate"
-    region      = "ca-central-1"
-    use_lockfile  = "true"
-    encrypt     = true
-  }
-}
