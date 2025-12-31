53b56265ef2c76b2769b58734c1137d6a4e5e0a5
kutt27 <satheesanamal6@gmail.com>
2025-12-31 10:09:28 +0530

    day 26 updated

---
+++ 26-Terraform-cloud/Code/main.tf
@@ -0,0 +1,30 @@
+# backend configuration
+terraform {
+  # terraform cli-workspace demo
+  cloud {
+    organization = "Test-Demo-123-1"
+    workspaces {
+      name = "test-demo-cli"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # locking the provider version to 6.x
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # the service will run on us-east-1
+}
+
+# create a s3 bucket
+resource "aws_s3_bucket" "first_s3_bucket" {
+  bucket = "amal-s3-bucket-terraform30days-1234" # name of the bucket, unique one
+
+  tags = {
+    Name        = "My bucket 2.0"
+    Environment = "Dev"
+  }
+}
---
+++ 26-Terraform-cloud/README.md
@@ -0,0 +1,3 @@
+# Day 26: Terraform Cloud and Workspaces
+
+Terraform Cloud is a cloud-based platform for managing and deploying infrastructure as code. It provides a central hub for storing and versioning Terraform configurations, managing state, and collaborating with team members. Terraform Cloud also offers features like remote execution, policy enforcement, and cost estimation.
